46de641c3b2f39b2ce897222ec366dc42848744a
AsamFernando <ferasam88@gmail.com>
2020-02-13 20:31:35 -0300

    trabajado en clase para hacer correcciones

--- comensalesDeAUno.py
+++ comensalesDeAUno.py
@@ -4,16 +4,16 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
-semaforoComensal=threading.Semaphore(3)
+semaforoComensal=threading.Semaphore(1)
 semaforoCocinero=threading.Semaphore(0)
 lockComensales=threading.Lock()
-comensales=10
+platosDisponibles = 3
+comensales=25
 
 class Cocinero(threading.Thread):
-  #def __init__(self, numero):
   def __init__(self):
     super().__init__()
-    self.name = 'Cocinero' #f'Cocinero {numero}'
+    self.name = 'Cocinero'
 
   def run(self):
     global platosDisponibles
@@ -23,9 +23,7 @@ class Cocinero(threading.Thread):
         logging.info('Reponiendo los platos...')
         platosDisponibles = 3
       finally:
-        for i in range(min(platosDisponibles, comensales)):
-          semaforoComensal.release()
-        lockComensales.release()
+        semaforoComensal.release()
 
 # semaforo para controlar la cantidad de platos correcta ya q el logging tarda mas que los comensales y la cantidad puede ser erronea
 # semaforo para q espere el comensal y despierte al cocinero
@@ -38,21 +36,25 @@ class Comensal(threading.Thread):
   def run(self):
     global platosDisponibles
     global comensales
-    semaforoComensal.acquire()
+    semaforoComensal.acquire() # el 4 comensal tiene q entrar a comer pero si no hay platos tiene q esperar al cocinero
     try:
-      platosDisponibles -= 1 
-      logging.info(f'Que rico! Quedan {platosDisponibles} platos')
-      comensales-=1
+      if platosDisponibles>0: 
+        platosDisponibles -= 1 
+        logging.info(f'Que rico! Quedan {platosDisponibles} platos')
+      if comensales>0:  
+        comensales-=1
     finally:
-      if platosDisponibles==0:
-        lockComensales.acquire()
+      if platosDisponibles==0 or comensales==0:
         semaforoCocinero.release()
+      else:
+        semaforoComensal.release()
+
+
+
         
-platosDisponibles = 3
 
 Cocinero().start()
-#Cocinero(1).start()
-#Cocinero(2).start()
+
 
 for i in range(comensales):
   Comensal(i).start()
